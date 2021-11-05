c8eec660449464c8d6c114c15a5f4964e5afbdf4
Wagner Willian <wag_willian@hotmail.com>
2021-11-05 13:29:38 -0300

    Fix Log Off Bug
    Fix the bug where the header was not being cleared on log off

--- RMDesktopUI.Library/Api/IAPIHelper.cs
+++ RMDesktopUI.Library/Api/IAPIHelper.cs
@@ -7,6 +7,7 @@ namespace RMDesktopUI.Library.Api
     public interface IAPIHelper
     {
         HttpClient ApiClient { get; }
+        void LogOffUser();
         Task<AuthenticatedUser> Authenticate(string username, string password);
 
         Task GetLoggedInUserInfo(string token);
--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using RMDesktopUI.EventModels;
+using RMDesktopUI.Library.Api;
 using RMDesktopUI.Library.Models;
 
 namespace RMDesktopUI.ViewModels
@@ -9,12 +10,14 @@ namespace RMDesktopUI.ViewModels
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
         private ILoggedInUserModel _user;
+        private IAPIHelper _apiHelper;
         
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, ILoggedInUserModel user)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, ILoggedInUserModel user, IAPIHelper apiHelper)
         {
             _events = events;
             _salesVM = salesVM;
             _user = user;
+            _apiHelper = apiHelper;
             
 
             _events.Subscribe(this);
@@ -41,7 +44,8 @@ namespace RMDesktopUI.ViewModels
 
         public void LogOut()
         {
-            _user.LogOffUser();
+            _user.ResetUserModel();
+            _apiHelper.LogOffUser();
             ActivateItem(IoC.Get<LoginViewModel>());
             NotifyOfPropertyChange(() => IsLoggedIn);
         }
