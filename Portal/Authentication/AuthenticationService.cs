237f5f1dc6e70d30ff7e8eb4f06542addeb392eb
Wagner Willian <wag_willian@hotmail.com>
2022-06-02 05:42:56 -0300

    Add Authentication system to the Portal

---
+++ Portal/Authentication/AuthenticationService.cs
@@ -0,0 +1,61 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Portal.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Portal.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _client;
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthenticationService(HttpClient client, AuthenticationStateProvider authStateProvider, ILocalStorageService localStorage)
+        {
+            _client = client;
+            _authStateProvider = authStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthentication)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>(key:"grant_type", value:"password"),
+                new KeyValuePair<string, string>("username", userForAuthentication.Email),
+                new KeyValuePair<string, string>("password", userForAuthentication.Password)
+            });
+
+            var authResult = await _client.PostAsync("https://localhost:5001/token", data); // Arrumar!!!!
+            var authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode == false)
+            {
+                return null;
+            }
+            var result = JsonSerializer.Deserialize<AuthenticatedUserModel>(
+                authContent,
+                options: new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            await _localStorage.SetItemAsync(key: "authToken", result.Access_Token);
+
+            ((AuthStateProvider)_authStateProvider).NotifyUserAuthentication(result.Access_Token);
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Access_Token);
+
+            return result;
+
+        }
+
+        public async Task Logout()
+        {
+            await _localStorage.RemoveItemAsync(key: "authToken");
+            ((AuthStateProvider)_authStateProvider).NotifyUserLogout();
+            _client.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
---
+++ Portal/Authentication/JwtParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Portal.Authentication
+{
+    
+    
+    
+    public static class JwtParser
+    {
+        public static IEnumerable<Claim> ParseClaimsFromJWT(string jwt)
+        {
+            var claims = new List<Claim>();
+            var payload = jwt.Split(separator: '.')[1];
+
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            ExtractRolesFromJWT(claims, keyValuePairs);
+
+            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+
+            return claims;
+        }
+
+        private static void ExtractRolesFromJWT(List<Claim> claims, Dictionary<string, object> keyValuePairs)
+        {
+            keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
+
+            if (roles is not null)
+            {
+                var parsedRoles = roles.ToString().Trim().TrimStart(trimChar: '[').TrimEnd(']').Split(separator: ',');
+
+                if (parsedRoles.Length > 1)
+                {
+                    foreach (var parsedRole in parsedRoles)
+                    {
+                        claims.Add(item: new Claim(ClaimTypes.Role, parsedRole.Trim(trimChar:'"')));
+                    }
+                }
+                else
+                {
+                    claims.Add(item: new Claim(ClaimTypes.Role, parsedRoles[0]));
+                }
+                keyValuePairs.Remove(ClaimTypes.Role);
+            }
+           
+        }
+        
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2:
+                    base64 += "==";
+                    break;
+                case 3:
+                    base64 += "=";
+                    break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
